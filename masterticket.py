94be933506eb0982f24135d8af40650ce6184173
Pat P <sell445@gmail.com>
2019-11-05 17:58:44 -0500

    finished refactoring lesson

--- masterticket.py
+++ masterticket.py
@@ -1,21 +1,23 @@
 TICKET_PRICE = 10
 
+SERVICE_CHARGE = 2
+
 tickets_remaining = 100
+
+def calculate_price(number_of_tickets):
+    return (number_of_tickets * TICKET_PRICE) + SERVICE_CHARGE
 
 while tickets_remaining > 0:
     print("There are {} tickets remaining for this event.".format(tickets_remaining))
     username = input("What is your name?  ")
-    # Expect and handle a ValueError
     try:
         tickets_needed = int(input("Hello {}, how many tickets do you need for this event?  ".format(username)))
-        # Raise Value Error if request is for more than available
         if tickets_needed > tickets_remaining:
             raise ValueError("There are only {} tickets remaining".format(tickets_remaining))
     except ValueError as err:
-        # Include error text
         print("Oh no, we ran into an issue. {}. Please try again".format(err))
     else:
-        total_price = tickets_needed * TICKET_PRICE
+        total_price = calculate_price(tickets_needed)
         print("The total cost for your tickets is ${}".format(total_price))
         confirmation = input("Do you wish to order {} tickets for ${} now?\nPlease enter Y or N  ".format(tickets_needed, total_price))
         if confirmation.lower() == 'y':
